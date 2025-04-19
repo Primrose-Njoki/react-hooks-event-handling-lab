e57e4c31663dea4ee6db431da8c534f52ce66a6e
Primrose <primrose.njoki@student.moringaschool.com>
2025-04-19 19:15:43 +0300

    complete event handling

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,21 @@
-// Code Keypad Component Here
+import React,{useState} from'react'
 
-function Keypad (){
+const Keypad=() =>{
+    const [password,setPassword] =useState('')//state management tracks password and initializes an empty string
+  //updates the password when inputs changes   
+    const handleChange =(event) =>{
+        setPassword(event.target.value)
+         console.log("Entering password...")
+    };
     return (
-        <div></div>
+        <div>
+            <input
+            type='password'
+            value='{password}'
+            onChange={handleChange}/>
+
+
+        </div>
     )
 }
 
